296e1593749ea448b2cac1f661ff8341865afddd
bKamilla <159395521+bKamilla@users.noreply.github.com>
2024-05-04 09:25:12 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 with open('input.txt', 'r') as input_:
-    lines = input_.readlines
+    lines = input_.readlines() 
 result_word = ''.join([line[0] for line in lines])
 with open('output.txt', 'w') as output_:
     output_.write(result_word)
