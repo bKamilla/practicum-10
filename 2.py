e76ef124c984638bdfa3c1062b677e72008e0350
bKamilla <159395521+bKamilla@users.noreply.github.com>
2024-05-04 09:16:44 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,6 @@
 with open('input.txt', 'r') as input_file:
     l = input_file.readlines()
 
-# Открываем файл для записи
 with open('output.txt', 'w') as output_file:
     for line in l:
         if line.strip().startswith('A'):
