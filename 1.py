13458a236be2ee2b84ba919c22b7d1c198965655
bKamilla <159395521+bKamilla@users.noreply.github.com>
2024-05-04 08:50:47 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,5 @@
-# Открываем файл input.txt для чтения
 with open('input.txt', 'r') as f:
     data = f.read()
-
-# Преобразуем данные к верхнему регистру
 data_upper = data.upper()
-
-# Открываем файл output.txt для записи
 with open('output.txt', 'w') as f:
     f.write(data_upper)
